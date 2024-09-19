05217f7d16cf2db5cc5d9422e9d0cf68b0db9569
Kevin Serrano <kevinserrano010@gmail.com>
2024-09-18 23:18:11 -0300

    Desafio 1 - React Native

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.parrafo}>
+        hola esta es mi primer Aplicacion Movil!
+      </Text>
+      <Text style={styles.parrafo}>Hola Coder!</Text>
+      <Button title="Presioname" onPress={() => alert("Hola Coder!")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#86D293",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  parrafo: {
+    color: "#000",
+    fontSize: 20,
+  },
+});
